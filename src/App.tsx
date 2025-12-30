7268b6029fa15f77f74eae3a07f68f55637f6c57
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-12-30 15:24:52 +0300

    feat(hero): implement auto-playing carousel with pagination indicators

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,15 @@ import HeroBanner from "./components/movies/HeroBanner";
 import MovieRow from "./components/movies/MovieRow";
 
 function App() {
-  // On récupère les films tendance pour le Hero
   const { movies: trendingMovies, loading: heroLoading } = useMovies("/trending/movie/week");
 
   return (
     <div className="min-h-screen bg-[#0a0a0a]">
       <Navbar />
-      <main className="pb-20">
-        {/* On passe le premier film de la liste au HeroBanner */}
+      <main>
+        {/* On passe le tableau entier movies au lieu d'un seul film */}
         <HeroBanner 
-          movie={trendingMovies[0] || null} 
+          movies={trendingMovies} 
           loading={heroLoading} 
         />
         
