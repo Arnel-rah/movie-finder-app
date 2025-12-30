5b9c2213552b5d9a53bbaadeba81812020f0ff30
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-12-30 14:55:25 +0300

    feat(ui): refine navbar layout with custom logo and specific branding colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
-import { useState } from "react";
-import Articles from "./components/Articles";
+// import { useState } from "react";
+
+import Navbar from "./components/layout/Navbar";
+
 
 function App() {
-  // Déclaration du state pour le nom
-  const [name, setName] = useState<string>(""); // valeur initiale vide
 
-  // Fonction pour gérer le changement de l'input
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Mon App</h1>
-      {/* Input lié au state */}
-      <input
-        type="text"
-        placeholder="Tape ton nom..."
-        value={name}
-        onChange={handleChange}
-        style={{ padding: "5px", marginBottom: "10px" }}
-      />
-
-      {/* On passe le state à Articles */}
-      <Articles name={name} age={24} />
-    </div>
+    <Navbar/>
   );
 }
 
