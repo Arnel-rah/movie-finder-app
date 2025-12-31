f160e328eb868a20ddf01052458d1f083aaf4f48
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-12-31 06:36:07 +0300

    feat(brands): add conditional left arrow and smooth dual navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import { useMovies } from "./hooks/useMovies";
 import Navbar from "./components/layout/Navbar";
 import HeroBanner from "./components/movies/HeroBanner";
+import BrandLogos from "./components/brands/BrandLogos"; // Importation
 import MovieRow from "./components/movies/MovieRow";
+import { useMovies } from "./hooks/useMovies";
 
 function App() {
-  // Récupère les films Tendance de la semaine (très varié)
-  const { movies: trendingMovies, loading: heroLoading } = useMovies("/trending/movie/week");
+  const { movies: trendingMovies, loading: heroLoading } = useMovies(
+    "/trending/movie/week"
+  );
 
   return (
     <div className="min-h-screen bg-[#0a0a0a]">
       <Navbar />
+
       <main>
-        <HeroBanner 
-          movies={trendingMovies} 
-          loading={heroLoading} 
-        />
-        
-        <MovieRow title="Just Release" endpoint="/movie/now_playing" />
-        <MovieRow title="Popular" endpoint="/movie/popular" />
+        <HeroBanner movies={trendingMovies} loading={heroLoading} />
+        <BrandLogos />
+        <div className="flex flex-col gap-8 pb-20">
+          <MovieRow title="Just Release" endpoint="/movie/now_playing" />
+          <MovieRow title="Popular" endpoint="/movie/popular" />
+        </div>
       </main>
     </div>
   );
