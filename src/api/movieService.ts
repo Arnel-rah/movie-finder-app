e0f486fc1b50cb8f9b04b7008b41caf99485fef2
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-12-31 14:16:56 +0300

    chore: setup environment variables and update gitignore

--- src/api/movieService.ts
+++ src/api/movieService.ts
@@ -1,9 +1,10 @@
 import type { Movie, MovieResponse } from "../types/movie";
 
-const API_KEY = '465990ebeb6469eea956020b02afedc7';
-const BASE_URL = 'https://api.themoviedb.org/3';
-export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
-export const BACKDROP_BASE_URL = 'https://image.tmdb.org/t/p/original';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
+
+export const IMAGE_BASE_URL = import.meta.env.VITE_TMDB_IMAGE_BASE_URL;
+export const BACKDROP_BASE_URL = import.meta.env.VITE_TMDB_BACKDROP_BASE_URL;
 
 export const fetchMovies = async (endpoint: string): Promise<Movie[]> => {
   const response = await fetch(`${BASE_URL}${endpoint}?api_key=${API_KEY}&language=fr-FR`);
